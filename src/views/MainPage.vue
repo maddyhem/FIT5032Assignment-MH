c9ed4d9a4a72e4a3434f97a33f893166c62abfb8
Madison Hem <mhem0008@student.monash.edu>
2026-08-29 14:16:36 +1000

    main contents skeleton

---
+++ src/views/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+
+
+
+<!-- MAIN CONTENTS -->
+    <div class="container mt-2 d-flex flex-column justify-content-center align-items-center vh-100">
+        <h1 class="text-center">Welcome to Cyclic Crafting</h1>
+            <div class="row mb-3">
+                <p> "There is no such thing as 'away'. When we throw anything away, it must go somewhere." - Annie Leonard</p>
+            </div>
+        <form>
+            <div class="row justify-content-center mb-3">
+                <div class="col-auto d-flex gap-2">
+                        <button type="submit" class="btn btn-secondary">About Us</button>
+                        <button type="submit" class="btn btn-secondary">Learn More</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
