aa9f70d4718eea11b2063a5b59f69cf35a6de6ca
Madison Hem <mhem0008@student.monash.edu>
2026-09-12 02:17:47 +1000

    added firebase config

--- src/main.js
+++ src/main.js
@@ -9,3 +9,21 @@ const app = createApp(App)
 
 app.use(router)
 app.mount('#app')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAP5HrJ7QW_Pp-6H0KOJMcg-VFN086g1C0",
+  authDomain: "fit5032-assignment-1cefe.firebaseapp.com",
+  projectId: "fit5032-assignment-1cefe",
+  storageBucket: "fit5032-assignment-1cefe.firebasestorage.app",
+  messagingSenderId: "578757617940",
+  appId: "1:578757617940:web:607509383402d29c367a01"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
